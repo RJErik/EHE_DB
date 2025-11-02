f6d7a660340ed45c38515e655b0a61213e25257c
RJErik <ritzerikjanos@gmail.com>
2025-11-02 14:05:14 +0100

    Renamed columns so it would be appropriate.
    There will be no encrypting for those columns.

--- Tables/api_key.sql
+++ Tables/api_key.sql
@@ -2,8 +2,8 @@ CREATE TABLE api_key (
     api_key_id INT GENERATED ALWAYS AS IDENTITY (START WITH 4438) PRIMARY KEY,
     user_id INT NOT NULL,
     platform_name VARCHAR(100) NOT NULL,
-    api_key_value_encrypt VARCHAR(255) NOT NULL,
-    secret_key_encrypt VARCHAR(255),
+    api_key_value VARCHAR(255) NOT NULL,
+    secret_key VARCHAR(255),
     date_added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     audit_created_by VARCHAR(255) NOT NULL DEFAULT current_setting('myapp.current_user', true),
     audit_created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -55,14 +55,14 @@ BEGIN
     END IF;
 
     INSERT INTO api_key_history (
-        api_key_id, user_id, platform_name, api_key_value_encrypt,
-        secret_key_encrypt, date_added, audit_created_by, audit_created_date,
+        api_key_id, user_id, platform_name, api_key_value,
+        secret_key, date_added, audit_created_by, audit_created_date,
         audit_updated_by, audit_updated_date,
         audit_version_number, history_dml_type,
         history_logged_date
     ) VALUES (
         entity_record.api_key_id, entity_record.user_id, entity_record.platform_name,
-        entity_record.api_key_value_encrypt, entity_record.secret_key_encrypt,
+        entity_record.api_key_value, entity_record.secret_key,
         entity_record.date_added, entity_record.audit_created_by, entity_record.audit_created_date,
         entity_record.audit_updated_by, entity_record.audit_updated_date,
         entity_record.audit_version_number,
